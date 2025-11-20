a5bd8ae5961fe21e9a4e95c8853f506ca0928edf
Ozan Güngör <82978076+ozanggnr@users.noreply.github.com>
2025-11-21 00:16:42 +0300

    Db Updated

--- APP/Domain/Db.cs
+++ APP/Domain/Db.cs
@@ -1,47 +1,120 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using static System.Formats.Asn1.AsnWriter;
 
 namespace APP.Domain
 {
+   
     public class Db : DbContext
     {
+       
+
+
+
+
+
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Db"/> class using the specified options.
+        /// </summary>
+        /// <param name="options">
+        /// The options to be used by the <see cref="DbContext"/>, such as the database provider and connection string (from appsettings.json).
+        /// </param>
         public Db(DbContextOptions options) : base(options)
         {
         }
 
-       
-        public DbSet<User> Users { get; set; }
-        public DbSet<Role> Roles { get; set; }      
-        public DbSet<Group> Groups { get; set; }     
-        public DbSet<UserRole> UserRoles { get; set; } 
 
-       
+
+        // Overriding OnModelCreating method is optional.
+        /// <summary>
+        /// Configures the entity relationships and database schema rules for the application domain.
+        /// This method defines how entities are related, sets up foreign key constraints, and customizes
+        /// the delete behavior for each relationship to prevent cascading deletes.
+        /// </summary>
+        /// <param name="modelBuilder">
+        /// The <see cref="ModelBuilder"/> used to configure entity mappings and relationships.
+        /// </param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // Index configurations:
+            // Defining unique indices to enforce uniqueness constraints on certain properties.
+            // CountryName data of the Conutries table can not have multiple same values.
+            modelBuilder.Entity<Country>().HasIndex(countryEntity => countryEntity.CountryName).IsUnique();
+
+            // CityName data of the Cities table can not have multiple same values.
+            modelBuilder.Entity<City>().HasIndex(cityEntity => cityEntity.CityName).IsUnique();
+
+            // Title data of the Groups table can not have multiple same values.
+            modelBuilder.Entity<Group>().HasIndex(groupEntity => groupEntity.Title).IsUnique();
+
+            // Name data of the Roles table can not have multiple same values.
+            modelBuilder.Entity<Role>().HasIndex(roleEntity => roleEntity.Name).IsUnique();
+
+            // UserName data of the Users table can not have multiple same values.
+            modelBuilder.Entity<User>().HasIndex(userEntity => userEntity.UserName).IsUnique();
+
+            // Defining indices for optimizing query performance on frequently searched properties.
+            modelBuilder.Entity<User>().HasIndex(userEntity => userEntity.CountryId);
+            modelBuilder.Entity<User>().HasIndex(userEntity => userEntity.CityId);
+
+            // Composite index on FirstName and LastName for optimizing searches involving both fields.
+            modelBuilder.Entity<User>().HasIndex(userEntity => new { userEntity.FirstName, userEntity.LastName });
+
+
+
+            // Relationship configurations:
+            // Configuration should start with the entities that have the foreign keys.
+            modelBuilder.Entity<City>()
+                .HasOne(cityEntity => cityEntity.Country) // each City entity has one related Country entity
+                .WithMany(countryEntity => countryEntity.Cities) // each Country entity has many related City entities
+                .HasForeignKey(cityEntity => cityEntity.CountryId) // the foreign key property in the City entity that
+                                                                   // references the primary key of the related Country entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a Country entity if there are related City entities
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(userRoleEntity => userRoleEntity.User) // each UserRole entity has one related User entity
+                .WithMany(userEntity => userEntity.UserRoles) // each User entity has many related UserRole entities
+                .HasForeignKey(userRoleEntity => userRoleEntity.UserId) // the foreign key property in the UserRole entity that
+                                                                        // references the primary key of the related User entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a User entity if there are related UserRole entities
+
+            modelBuilder.Entity<UserRole>()
+                .HasOne(userRoleEntity => userRoleEntity.Role) // each UserRole entity has one related Role entity
+                .WithMany(roleEntity => roleEntity.UserRoles) // each Role entity has many related UserRole entities
+                .HasForeignKey(userRoleEntity => userRoleEntity.RoleId) // the foreign key property in the UserRole entity that
+                                                                        // references the primary key of the related Role entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a Role entity if there are related UserRole entities
+
+            modelBuilder.Entity<User>()
+                .HasOne(userEntity => userEntity.Country) // each User entity has one related Country entity
+                .WithMany(countryEntity => countryEntity.Users) // each Country entity has many related User entities
+                .HasForeignKey(userEntity => userEntity.CountryId) // the foreign key property in the User entity that
+                                                                   // references the primary key of the related Country entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a Country entity if there are related User entities
+
+            modelBuilder.Entity<User>()
+                .HasOne(userEntity => userEntity.City) // each User entity has one related City entity
+                .WithMany(cityEntity => cityEntity.Users) // each City entity has many related User entities
+                .HasForeignKey(userEntity => userEntity.CityId) // the foreign key property in the User entity that
+                                                                // references the primary key of the related City entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a City entity if there are related User entities
+
+            modelBuilder.Entity<User>()
+                .HasOne(userEntity => userEntity.Group) // each User entity has one related Group entity
+                .WithMany(groupEntity => groupEntity.Users) // each Group entity has many related User entities
+                .HasForeignKey(userEntity => userEntity.GroupId) // the foreign key property in the User entity that
+                                                                 // references the primary key of the related Group entity
+                .OnDelete(DeleteBehavior.NoAction); // prevents deletion of a Group entity if there are related User entities
+
             
-            modelBuilder.Entity<UserRole>()
-                .HasKey(ur => new { ur.UserId, ur.RoleId });
-
-           
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.User)
-                .WithMany(u => u.UserRoles)
-                .HasForeignKey(ur => ur.UserId);
-
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.Role)
-                .WithMany(r => r.UserRoles)
-                .HasForeignKey(ur => ur.RoleId);
-
-          
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.Group)
-                .WithMany(g => g.Users)
-                .HasForeignKey(u => u.GroupId);
-
-           
-            modelBuilder.Entity<User>()
-                .Property(u => u.Score)
-                .HasColumnType("decimal(18,2)");
         }
     }
 }
---
+++ APP/Migrations/20251120211126_v2.cs
@@ -0,0 +1,286 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Score",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserRoles",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "UserRoles",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "Roles",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "Groups",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CountryName = table.Column<string>(type: "TEXT", maxLength: 125, nullable: false),
+                    Guid = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CityName = table.Column<string>(type: "TEXT", maxLength: 175, nullable: false),
+                    CountryId = table.Column<int>(type: "INTEGER", nullable: false),
+                    FilePath = table.Column<string>(type: "TEXT", nullable: true),
+                    Guid = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cities_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CityId",
+                table: "Users",
+                column: "CityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CountryId",
+                table: "Users",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_FirstName_LastName",
+                table: "Users",
+                columns: new[] { "FirstName", "LastName" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_UserName",
+                table: "Users",
+                column: "UserName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_Name",
+                table: "Roles",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_Title",
+                table: "Groups",
+                column: "Title",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CityName",
+                table: "Cities",
+                column: "CityName",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cities_CountryId",
+                table: "Cities",
+                column: "CountryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_CountryName",
+                table: "Countries",
+                column: "CountryName",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Cities_CityId",
+                table: "Users",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Countries_CountryId",
+                table: "Users",
+                column: "CountryId",
+                principalTable: "Countries",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Cities_CityId",
+                table: "Users");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Countries_CountryId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Cities");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CityId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CountryId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_FirstName_LastName",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_UserName",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserRoles_UserId",
+                table: "UserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roles_Name",
+                table: "Roles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Groups_Title",
+                table: "Groups");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "Groups");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Score",
+                table: "Users",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                columns: new[] { "UserId", "RoleId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Roles_RoleId",
+                table: "UserRoles",
+                column: "RoleId",
+                principalTable: "Roles",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserRoles_Users_UserId",
+                table: "UserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
